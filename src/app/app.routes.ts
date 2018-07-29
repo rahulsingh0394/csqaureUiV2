39bf169849fcb83e9f9e696cf16da10b6ee7cb87
Rahul Singh <rahul@bellwether.org.in>
2018-07-29 12:02:17 +0530

    Initial Commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+// import { AdminComponent } from './admin/admin.component';
+// import { StudentComponent } from './student/student.component';
+// import { AppComponent } from '../app/app.component';
+
+export const Router: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    //   { path: 'about', loadChildren: './about/about.module#AboutModule' },
+    //   { path: 'bangalore', component: BangaloreComponent },
+    //   { path: 'contact', loadChildren: './contact/contact.module#ContactModule' },
+    //   { path: 'careers', loadChildren: './careers/careers.module#CareersModule' },
+    //   { path: 'media', loadChildren: './media/media.module#MediaModule' },
+    //   { path: 'parents-review', loadChildren: './parents/parents.module#ParentsModule' },
+    //   { path: 'privacy', loadChildren: './privacy/privacy.module#PrivacyModule' },
+    //   { path: 'question', loadChildren: './question/ques.module#QuesModule' },
+    //   { path: 'signup', loadChildren: './signup/signup.module#SignupModule' },
+    //   { path: 'studentForm', component: StudentComponent },
+    //   { path: 'tutorForm', component: TutorComponent },
+    //   { path: 'terms', loadChildren: './terms/terms.module#TermsModule'},
+    //   { path: 'tutors-review', loadChildren: './tutors/tutors.module#TutorsModule' },
+    //   { path: 'admin-app',      loadChildren: './admin-app/admin-app.module#AdminAppModule' },
+    //   { path: 'home-tutors-private-tutors', component: HomeTutorComponent },
+    //   { path: '', loadChildren: './Class-wise-best-private-home-tutor/Class-wise-best-private-home-tutor.module#ClassWiseModule' },
+    //   { path: '', loadChildren: './Syllabus-wise-best-private-home-tutor/Syllabus-wise-best-private-home-tutor.module#SyllabusWiseModule' } ,
+    //   { path: '', loadChildren: './Subjects-wise-best-private-home-tutor/Subjects-wise-best-private-home-tutor.module#SubjectsWiseModule' }    
+];
+
+
